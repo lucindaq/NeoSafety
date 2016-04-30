02b679a9893f427c9021eeaefa6e82a41dd66b14
Lucinda Quintal <lucinda@merl.us>
2016-04-30 07:45:36 -0700

    fixing iOS icons

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,28 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "id": "cordova-plugin-geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "id": "cordova-plugin-geolocation.PositionError",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-geolocation": "2.1.0",
+    "cordova-plugin-whitelist": "1.2.1"
+};
+// BOTTOM OF METADATA
+});

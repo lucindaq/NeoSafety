39b22de3e4fd57c87015d77774a74baa58beb67d
Lucinda Quintal <lucinda@merl.us>
2016-06-12 12:18:11 -0700

    cleared jqplot when updated

--- www/js/crime_stats.js
+++ www/js/crime_stats.js
@@ -173,7 +173,8 @@ function crimeResult (position) {
 			}
 
     function plotCrimeStats(data) {
-			$.jqplot.config.enablePlugins = true;
+		$("#crime-chart").empty();
+		$.jqplot.config.enablePlugins = true;
 
 			var plot1 = $.jqplot('crime-chart', [data], {
 		        gridPadding: {

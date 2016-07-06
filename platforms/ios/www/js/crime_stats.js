dbf20eece7c29e02b6c458da7c20341c3e787178
Lucinda Quintal <lucinda@merl.us>
2016-07-06 01:13:41 -0700

    testing geocomplete fix for android

--- platforms/ios/www/js/crime_stats.js
+++ platforms/ios/www/js/crime_stats.js
@@ -30,6 +30,7 @@
 
 
 function crimeResult (position, searched) {
+	$('#brian').html('lat/lng: ' + JSON.stringify(position));
 				var murder = ["'09A'", "'09B'", "'09C'"];
 				var theft = ["'120'", "'220'", "'23D'", "'23F'", "'23G'", "'23H'", "'240'", "'280'", "'BURGLARY'", "'LARCENY/THEFT'", "'ROBBERY'", "'STOLEN PROPERTY'", "'VEICHLE THEFT'"];
 				var subAbuse = ["'35A'", "'35B'", "'90D'", "'90E'", "'90G'", "'DRIVING UNDER THE INFLUENCE'", "'DRUG/NARCOTIC'", "'DRUNKENNESS'", "'LIQUOR LAWS'"];
